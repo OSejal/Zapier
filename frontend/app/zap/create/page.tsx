7b13beb4808a61b8101f05f0602813abc206ce78
Sejal <106868816+Sejal1411@users.noreply.github.com>
2025-10-29 22:30:54 +0530

    Next commit

--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -1,32 +1,63 @@
 "use client";
 
+import { BACKEND_URL } from "@/app/config";
 import { Appbar } from "@/components/Appbar";
 import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 import { ZapCell } from "@/components/ZapCell";
-import { useState } from "react";
+import axios from "axios";
+import { use, useEffect, useState } from "react";
+
+function useAvailableActionsAndTrigger() {
+    const [availableActions, setAvailableActions] = useState([]);
+    const [availableTriggers, setAvailableTriggers] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/trigger/available`)
+            .then(x =>setAvailableTriggers(x.data.availableTriggers))
+
+        axios.get(`${BACKEND_URL}/api/v1/action/available`)
+            .then(x => setAvailableActions(x.data.availableActions));
+    }, [])
+
+    return {
+        availableActions,
+        availableTriggers
+    }
+}
 
 export default function() {
-    const [selectedTrigger, setSelectedTrigger] = useState("");
+    const 
+    const [selectedTrigger, setSelectedTrigger] = useState<{
+        id: string;
+        name: string;
+    }>();
     const [selectedActions, setSelectedActions] = useState<{
+        index: number;
         availableActionId: string;
         availableActionName: string;
     }[]>([]);
+    const [selectedModalIndex, setSelectedModalIndex] = useState<null | number>(null);
 
     return <div>
         <Appbar />
         <div className="w-full min-h-screen bg-slate-300 flex flex-col justify-center pt-[-40px]">
             <div className="flex justify-center w-full">
-                <ZapCell name={selectedTrigger ? selectedTrigger : "Trigger"} index={1}/>
+                <ZapCell onClick = {() => {
+                    setSelectedModalIndex(1);
+                }} name={selectedTrigger?.name ? selectedTrigger.name : "Trigger"} index={1}/>
             </div>
             <div className="w-full pt-2 pb-2">
                 {selectedActions.map((action, index) => <div className="flex justify-center">
-                 <ZapCell name={action ?
-                 action.availableActionName : "Action"} index={2 + index} /> </div>)}
+                <ZapCell onClick = {() => {
+                    setSelectedModalIndex(action.index);
+                }} name={action.availableActionName ? action.availableActionName : "Action"} 
+                 index={action.index} /> </div>)}
             </div>
             <div className="flex justify-center">
-                <div className="w-4">
+                <div>
                     <PrimaryButton onClick={() => {
                         setSelectedActions(a => [...a, {
+                            index: a.length + 2,
                             availableActionId: "",
                             availableActionName: ""
                         }])
@@ -36,5 +67,62 @@ export default function() {
                 </div>
             </div>
         </div>
+        {selectedModalIndex &&  <Modal onSelect={(props: null | { name: string; id: string; }) => {
+            if (props === null) {
+                setSelectedModalIndex(null);
+                return;
+            }
+            if(selectedModalIndex === 1) {
+                setSelectedTrigger({
+                    id: props.id,
+                    name: props.name
+                })
+            } else {
+                setSelectedActions(a => {
+                    let newActions = [...a];
+
+                    newActions[selectedModalIndex - 2] = {
+                        index: selectedModalIndex,
+                        availableActionId: props.id,
+                        availableActionName: props.name
+                    }
+
+                    return newActions
+                })
+            }
+            setSelectedModalIndex(null);
+        }} index={selectedModalIndex} />}
     </div>
 } 
+
+function Modal({ index, onSelect }: { index: number, onSelect: (props: null | { name: string; id: 
+    string; }) => void }) {
+    return <div className=" fixed top-0 right-0 left-0 z-50 justify-center items-center w-full 
+    md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-100 bg-opacity-70 flex">
+            <div className="relative p-4 w-full max-w-2xl max-h-full">
+                <div className="relative bg-white rounded-lg shadow-sm">
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                        <div className="text-xl">
+                           Select {index === 1 ? "Trigger" : "Action"} 
+                        </div>
+                            <button onClick={() => {
+                                onSelect(null);
+                            }} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 
+                            hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center 
+                            items-center" data-modal-hide="default-modal">
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" 
+                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <div className="p-4 md:p-5 space-y-4">
+                            zap and zaps page
+                        </div>
+                    </div>
+                </div>
+    </div>
+
+}
--- primary-backend/src/router/user.ts
+++ primary-backend/src/router/user.ts
@@ -4,6 +4,7 @@ import { SigninSchema, SignupSchema } from "../types";
 import { prismaClient } from "../db";
 import jwt from "jsonwebtoken";
 import { JWT_PASSWORD } from "../config";
+
 const router = Router();
 
 router.post("/signup", async (req, res) => {
